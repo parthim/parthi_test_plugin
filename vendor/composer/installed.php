efe16722ec351265c7be4d2076c7f80d454288b6
parthiban <parthiban.tealorca.in>
2021-02-24 16:24:33 +0530

    Composer Created and autoload is used to include all the php files to parthi.php and namespace is used instead of require_once

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ac99e1f320f0309b06433d1c493843384baf120c',
+    'name' => 'parthi/parthi',
+  ),
+  'versions' => 
+  array (
+    'parthi/parthi' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ac99e1f320f0309b06433d1c493843384baf120c',
+    ),
+  ),
+);
